eedc57c805cb3c91d6bc16ac12fb039fa73e4862
Zeynel KOZAK <zeynell.kozak@outlook.com>
2025-02-09 18:43:58 +0300

    create host-1 app

--- host-1/vite.config.ts
+++ host-1/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import federation from "@originjs/vite-plugin-federation";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    federation({
+      name: "host-1",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Host1": "./src/App.vue",
+      },
+      shared: ["vue"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+  },
+  server: {
+    port: 5001,
+  },
+});
